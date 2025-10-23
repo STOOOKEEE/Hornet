02f7b7c69ed61d1d2754158f6abebb3abc4e174b
STOOOKEEE <armand.sechon@edu.devinci>
2025-10-23 09:42:03 +0200

    m

--- Backend/pages/dashboard.tsx
+++
@@ -1,6 +0,0 @@
-import { DashboardPage } from "../components/DashboardPage";
-
-export default function Dashboard() {
-  // On n'a pas besoin de onBackToHome ici, car ce sera une page dédiée
-  return <DashboardPage onBackToHome={() => {}} />;
-}
